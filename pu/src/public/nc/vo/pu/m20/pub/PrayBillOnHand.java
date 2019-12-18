7eb1868d3bbf88fc1ec2a26a3176f59bd238946c
pusilin <psl123456>
2019-12-18 19:44:34 +0800

    1

--- pu/src/public/nc/vo/pu/m20/pub/PrayBillOnHand.java
+++ pu/src/public/nc/vo/pu/m20/pub/PrayBillOnHand.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import nc.bs.framework.common.NCLocator;
 import nc.itf.uap.IUAPQueryBS;
+import nc.jdbc.framework.processor.MapListProcessor;
 import nc.jdbc.framework.processor.MapProcessor;
 import nc.pubitf.ic.onhand.IOnhandQry;
 import nc.vo.ic.onhand.entity.OnhandVO;
@@ -28,30 +29,40 @@ public class PrayBillOnHand extends NcInnerFunction {
 			return "-1";
 		}
 		IUAPQueryBS bs = NCLocator.getInstance().lookup(IUAPQueryBS.class);
-		Map<String,Object>  map =null;
+		List<Map<String,Object>>  list =null;
 		try {
 			String materialCode = (String) param.get(0);
 			String pk_org=(String) param.get(1);
-			String sql =" select  a.pk_stordoc,b.pk_material from  bd_materialstock a ,bd_material b where a.pk_material=b.pk_material and b.code='"+materialCode+"' and a.pk_org='"+pk_org+"'";
-			map=(Map<String, Object>) bs.executeQuery(sql, new MapProcessor());
-			String pk_stordoc=String.valueOf(map.get("pk_stordoc"));
-			String pk_material=String.valueOf(map.get("pk_material"));
-			IOnhandQry onhandQuery = NCLocator.getInstance().lookup(IOnhandQry.class);
-//			String dims[] = { "pk_org", "cwarehouseid", "cmaterialvid" };
-			String dims[] = { "pk_org", "cmaterialvid" };
-			OnhandQryCond cond = new OnhandQryCond();
-			cond.addSelectFields(dims);
-			cond.setISSum(true);
-//			cond.addFilterDimConditon(dims, new Object[] { pk_org, pk_stordoc, pk_material });
-			cond.addFilterDimConditon(dims, new Object[] { pk_org, pk_material });
-			OnhandVO[] vos = onhandQuery.queryOnhand(cond);
-			if(vos.length<1){
-				return "无";
-			}else{
-				UFDouble natpnum=vos[0].getNonhandnum();
-				if(natpnum.toDouble()>0){
-					return "有";
+			String materialtypeSql="select materialtype from bd_material where code='"+materialCode+"' and pk_org='"+pk_org+"'";
+			Map<String,Object> typeMap=(Map<String, Object>) bs.executeQuery(materialtypeSql, new MapProcessor());
+			String type=typeMap.get("materialtype").toString();
+			String sql =" select  a.pk_stordoc,b.pk_material from  bd_materialstock a ,bd_material b where a.pk_material=b.pk_material and b.materialtype='"+type+"' and a.pk_org='"+pk_org+"'";
+			list=(List<Map<String, Object>>) bs.executeQuery(sql, new MapListProcessor() );
+			UFDouble natpnum=new UFDouble();
+			for(int i =0;i<list.size();i++){
+				Map<String,Object> map=list.get(i);
+//				String pk_stordoc=String.valueOf(map.get("pk_stordoc"));
+				String pk_material=String.valueOf(map.get("pk_material"));
+				IOnhandQry onhandQuery = NCLocator.getInstance().lookup(IOnhandQry.class);
+//				String dims[] = { "pk_org", "cwarehouseid", "cmaterialvid" };
+				String dims[] = { "pk_org", "cmaterialvid" };
+				OnhandQryCond cond = new OnhandQryCond();
+				cond.addSelectFields(dims);
+				cond.setISSum(true);
+//				cond.addFilterDimConditon(dims, new Object[] { pk_org, pk_stordoc, pk_material });
+				cond.addFilterDimConditon(dims, new Object[] { pk_org, pk_material });
+				OnhandVO[] vos = onhandQuery.queryOnhand(cond);
+				if(vos==null){
+					continue;
 				}
+				if(vos.length<1){
+					continue;
+				}else{
+					natpnum=natpnum.add(vos[0].getNonhandnum());
+				}
+			}
+			if(natpnum.toDouble()>0){
+				return "有";
 			}
 			return "无";
 		} catch (BusinessException e) {
