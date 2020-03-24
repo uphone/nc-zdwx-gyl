9079fd794e34454c52a8a1b7bb148c38d4a3d9a2
uphone <595228808@qq.com>
2020-03-24 16:59:14 +0800

    optimize

--- ct/src/private/nc/bs/ct/busireg/PurdailyValidateListener.java
+++ ct/src/private/nc/bs/ct/busireg/PurdailyValidateListener.java
@@ -1,7 +1,9 @@
 package nc.bs.ct.busireg;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import nc.bs.businessevent.BusinessEvent;
 import nc.bs.businessevent.IBusinessEvent;
@@ -45,7 +47,6 @@ public class PurdailyValidateListener implements IBusinessListener {
 		PayPlanVO last = payPlanVOs[payPlanVOs.length-1];
 		UFDouble lastR = last.getNrate();
 		
-		List<String> sqls = new ArrayList<>(16);
 		UFDouble preTotal = UFDouble.ZERO_DBL;
 		for(int i = 0;i<payPlanVOs.length;i++) {
 			if(i == payPlanVOs.length-1) break;
@@ -55,39 +56,47 @@ public class PurdailyValidateListener implements IBusinessListener {
 			UFDouble naccumpaymny = payPlanVO.getNaccumpaymny();
 			UFDouble s = nmny.sub(naccumpaymny == null ? UFDouble.ZERO_DBL : naccumpaymny); // 付款计划中的剩余付款金额
 			UFDouble r = payPlanVO.getNrate(); // 付款比例
-			if(s.doubleValue()==0) {
+			UFDouble a = s.add(d.multiply(r).div(100)); // 付款计划中调整后的金额(a = s + d * r)
+			if(s.doubleValue()==0 || a.doubleValue() < 0) {
 				preTotal = preTotal.add(nmny);
 				lastR = lastR.add(r);
-				sqls.add("update CT_PAYPLAN set NTOTALORIGMNY="+payPlanVO.getNtotalorigmny()+" where PK_CT_PAYPLAN='"+payPlanVO.getPk_ct_payplan()+"'");
-				continue;
-			}
-			// a = s + d * r
-			UFDouble a = s.add(d.multiply(r).div(100));
-			if(a.doubleValue() < 0) {
+				//sqls.add("update CT_PAYPLAN set NTOTALORIGMNY="+payPlanVO.getNtotalorigmny()+" where PK_CT_PAYPLAN='"+payPlanVO.getPk_ct_payplan()+"'");
+			}/*else if(a.doubleValue() < 0) {
 				preTotal = preTotal.add(nmny);
 				lastR = lastR.add(r);
-				sqls.add("update CT_PAYPLAN set NTOTALORIGMNY="+payPlanVO.getNtotalorigmny()+" where PK_CT_PAYPLAN='"+payPlanVO.getPk_ct_payplan()+"'");
-				continue;
+//				sqls.add("update CT_PAYPLAN set NTOTALORIGMNY="+payPlanVO.getNtotalorigmny()+" where PK_CT_PAYPLAN='"+payPlanVO.getPk_ct_payplan()+"'");
+			}*/else{
+				preTotal = preTotal.add(a);
+				payPlanVO.setNmny(a);
+				payPlanVO.setNorigmny(a);
 			}
-			preTotal = preTotal.add(a);
-			payPlanVO.setNmny(a);
-			payPlanVO.setNorigmny(a);
-			sqls.add("update CT_PAYPLAN set NMNY="+payPlanVO.getNmny()+",NORIGMNY="+payPlanVO.getNorigmny()+",NTOTALORIGMNY="+payPlanVO.getNtotalorigmny()+" where PK_CT_PAYPLAN='"+payPlanVO.getPk_ct_payplan()+"'");
+			//sqls.add("update CT_PAYPLAN set NMNY="+payPlanVO.getNmny()+",NORIGMNY="+payPlanVO.getNorigmny()+",NTOTALORIGMNY="+payPlanVO.getNtotalorigmny()+" where PK_CT_PAYPLAN='"+payPlanVO.getPk_ct_payplan()+"'");
 		}
 //		UFDouble lastS = last.getNmny().sub(last.getNaccumpaymny() == null ? UFDouble.ZERO_DBL : last.getNaccumpaymny());
 //		UFDouble lastA = lastS.add(d.multiply(lastR).div(100));
+		List<String> sqls = new ArrayList<>(16);
 		UFDouble lastA = ctMny.sub(preTotal);
 		if(lastA.doubleValue() < 0) {
-			throw new BusinessException("付款计划金额调整失败，请检查。");
+			// 将负数金额累加到上一行,并将最后一行删除
+			PayPlanVO currLastVO = payPlanVOs[payPlanVOs.length-2];
+			currLastVO.setNmny(currLastVO.getNmny().add(lastA));
+			currLastVO.setNorigmny(currLastVO.getNorigmny().add(lastA));
+			sqls.add("delete from CT_PAYPLAN where PK_CT_PAYPLAN='"+last.getPk_ct_payplan()+"'");
+			//throw new BusinessException("付款计划金额调整失败，请检查。");
+		}else{
+			last.setNtotalorigmny(ctMny);
+			last.setNmny(lastA);
+			last.setNorigmny(lastA);
+			sqls.add("update CT_PAYPLAN set NMNY="+last.getNmny()+",NORIGMNY="+last.getNorigmny()+",NTOTALORIGMNY="+last.getNtotalorigmny()+" where PK_CT_PAYPLAN='"+last.getPk_ct_payplan()+"'");
 		}
-		last.setNtotalorigmny(ctMny);
-		last.setNmny(lastA);
-		last.setNorigmny(lastA);
-		sqls.add("update CT_PAYPLAN set NMNY="+last.getNmny()+",NORIGMNY="+last.getNorigmny()+",NTOTALORIGMNY="+last.getNtotalorigmny()+" where PK_CT_PAYPLAN='"+last.getPk_ct_payplan()+"'");
+		for(int i = 0;i<payPlanVOs.length;i++) {
+			if(i == payPlanVOs.length-1) break;
+			PayPlanVO payPlanVO = payPlanVOs[i];
+			sqls.add("update CT_PAYPLAN set NMNY="+payPlanVO.getNmny()+",NORIGMNY="+payPlanVO.getNorigmny()+",NTOTALORIGMNY="+payPlanVO.getNtotalorigmny()+" where PK_CT_PAYPLAN='"+payPlanVO.getPk_ct_payplan()+"'");			
+		}
 		IRewritePayPlan payPlanRewriteService = NCLocator.getInstance().lookup(IRewritePayPlan.class);
 		for(int i = 0;i<sqls.size();i++) {
 			payPlanRewriteService.updatePayPlanBySql(sqls.get(i));
 		}
 	}
-
 }
