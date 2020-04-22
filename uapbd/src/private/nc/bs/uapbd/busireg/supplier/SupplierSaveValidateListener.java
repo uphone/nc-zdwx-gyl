9453ad543465c25d1e2bf27507ca2739a55ed32e
uphone <595228808@qq.com>
2020-04-23 00:03:20 +0800

    opt

---
+++ uapbd/src/private/nc/bs/uapbd/busireg/supplier/SupplierSaveValidateListener.java
@@ -0,0 +1,22 @@
+package nc.bs.uapbd.busireg.supplier;
+
+import nc.bs.businessevent.IBusinessEvent;
+import nc.bs.businessevent.IBusinessListener;
+import nc.bs.businessevent.bd.BDCommonEvent;
+import nc.vo.bd.supplier.SupLinkmanVO;
+import nc.vo.bd.supplier.SupplierVO;
+import nc.vo.pub.BusinessException;
+
+public class SupplierSaveValidateListener implements IBusinessListener {
+
+	@Override
+	public void doAction(IBusinessEvent event) throws BusinessException {
+		BDCommonEvent be = (BDCommonEvent) event;
+		Object[] objs = be.getNewObjs();
+		SupplierVO supVO = (SupplierVO) objs[0];
+		SupLinkmanVO[] links = supVO.getSuplinkman();
+		if (links == null || links.length == 0)
+			throw new BusinessException("请增加联系人");
+	}
+
+}
